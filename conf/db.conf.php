4825dbb75d84939fad0b53dc95325fd67bcda98f
genice <chenlin_wsfd@163.com>
2017-04-18 09:23:19 +0800

    dkjdkf

---
+++ conf/db.conf.php
@@ -0,0 +1,13 @@
+<?php
+
+$conf = array(
+	'db' => array(
+		'host'     => '127.0.0.1',
+		'user'     => 'root',
+		'password' => 'root',
+		'dbname'   => 'Ishow',
+		'encoding' => 'utf8',
+	),
+);
+
+?>
